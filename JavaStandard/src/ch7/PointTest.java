a5493778f5c19b2a42fe58664e7c39879f1f4cab
ksnd0297 <ksnd0297@gmail.com>
2022-03-09 23:23:01 +0900

    22/03/09

---
+++ JavaStandard/src/ch7/PointTest.java
@@ -0,0 +1,45 @@
+package ch7;
+
+public class PointTest {
+	public static void main(String[] args) {
+		Point3D p3 = new Point3D(1, 2, 3);
+		Point3D p2 = new Point3D();
+		System.out.println(p2.getLocation());
+	}
+}
+
+class Point {
+	int x;
+	int y;
+
+	Point() {
+		this(10, 20);
+	}
+
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	String getLocation() {
+		return "x : " + x + ", y : " + y;
+	}
+}
+
+class Point3D extends Point {
+	int z;
+
+	Point3D() {
+		super(100, 200);
+		this.z = 300;
+	}
+
+	Point3D(int x, int y, int z) {
+		super(x, y);
+		this.z = z;
+	}
+
+	String getLocation() {
+		return "x : " + x + ", y : " + y + ", z : " + z;
+	}
+}
