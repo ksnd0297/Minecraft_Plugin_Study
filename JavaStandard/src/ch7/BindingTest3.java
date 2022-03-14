950afe46ce6dd8bfd8570211551e3a8447fff79c
ksnd0297 <ksnd0297@gmail.com>
2022-03-14 17:36:01 +0900

    22/03/14

--- JavaStandard/src/ch7/BindingTest3.java
+++ JavaStandard/src/ch7/BindingTest3.java
@@ -25,6 +25,7 @@ class Child extends Parent {
 	int x = 200;
 
 	void method() {
+		System.out.println("Child Method");
 		System.out.println("x = " + x);
 		System.out.println("super.x = " + super.x);
 		System.out.println("this.x = " + this.x);
