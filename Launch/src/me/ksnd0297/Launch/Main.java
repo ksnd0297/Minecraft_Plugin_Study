fa610f64e6731fc8d6b25890740c6e5bbf436cb2
ksnd0297 <ksnd0297@gmail.com>
2022-04-03 22:59:55 +0900

    22/04/03

--- Launch/src/me/ksnd0297/Launch/Main.java
+++ Launch/src/me/ksnd0297/Launch/Main.java
@@ -1,7 +1,9 @@
 package me.ksnd0297.Launch;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -16,6 +18,18 @@ public class Main extends JavaPlugin {
 	}
 
 	public boolean onCommnad(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("launch")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("console goes flaying");
+				return true;
+			}
+			Player player = (Player) sender;
+			if (args.length == 0) {
+				// launch
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Zoooooooom!");
+				player.setVelocity(player.getLocation().getDirection());
+			}
+		}
 		return false;
 	}
 
